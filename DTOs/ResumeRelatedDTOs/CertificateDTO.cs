2e4f33c4ac5e68da45614ba2bea6b0cc66bde659
georgikk314 <gogomen2005@gmail.com>
2023-07-07 02:46:44 +0300

    make the validation for certificates

--- DTOs/ResumeRelatedDTOs/CertificateDTO.cs
+++ DTOs/ResumeRelatedDTOs/CertificateDTO.cs
@@ -1,9 +1,16 @@
-﻿namespace Online_CV_Builder.DTOs.ResumeRelatedDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_CV_Builder.DTOs.ResumeRelatedDTOs
 {
     public class CertificateDTO
     {
+
         public int ResumeId { get; set; }
+        [Required(ErrorMessage = "Certificate Name is required.")]
+        [StringLength(100, ErrorMessage = "Certificate Name cannot exceed 100 characters.")]
         public string CertificateName { get; set; }
+
+        [StringLength(100, ErrorMessage = "Issuing Organization cannot exceed 100 characters.")]
         public string IssuingOrganization { get; set; }
         public DateTime IssueDate { get; set; }
     }
