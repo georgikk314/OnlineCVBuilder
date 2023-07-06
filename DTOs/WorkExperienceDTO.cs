dcf21cada6378ddd8d0c14655b87b66004ff065d
georgikk314 <gogomen2005@gmail.com>
2023-07-07 01:36:00 +0300

    add WorkExperienceDto

---
+++ DTOs/WorkExperienceDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Online_CV_Builder.DTOs
+{
+    public class WorkExperienceDTO
+    {
+        public int ResumeId { get; set; }
+        public string CompanyName { get; set; }
+        public string Position { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+    }
+}
