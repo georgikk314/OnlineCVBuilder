61b247be4712e0b7ab56d3d5cf3f0e3212220e37
georgikk314 <gogomen2005@gmail.com>
2023-07-07 02:50:27 +0300

    make the validation for skill

--- DTOs/ResumeRelatedDTOs/SkillDTO.cs
+++ DTOs/ResumeRelatedDTOs/SkillDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace Online_CV_Builder.DTOs.ResumeRelatedDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_CV_Builder.DTOs.ResumeRelatedDTOs
 {
     public class SkillDTO
     {
         public int ResumeId { get; set; }
+
+        [Required(ErrorMessage = "Skill Name is required.")]
+        [StringLength(200, ErrorMessage = "Skill Name cannot exceed 200 characters.")]
         public string SkillName { get; set; }
     }
 }
