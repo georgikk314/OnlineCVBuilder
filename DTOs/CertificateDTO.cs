42ec2133f24abd5573d83cb1aee1295739edb1a3
georgikk314 <gogomen2005@gmail.com>
2023-07-07 01:44:38 +0300

    add Certificate dto

---
+++ DTOs/CertificateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Online_CV_Builder.DTOs
+{
+    public class CertificateDTO
+    {
+        public int ResumeId { get; set; }
+        public string CertificateName { get; set; }
+        public string IssuingOrganization { get; set; }
+        public DateTime IssueDate { get; set; }
+    }
+}
