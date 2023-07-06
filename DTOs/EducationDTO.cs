8e377c5f1fe3b0ebaa59dde3bc78519e9756c721
georgikk314 <gogomen2005@gmail.com>
2023-07-07 01:41:52 +0300

    add Education dto

---
+++ DTOs/EducationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Online_CV_Builder.DTOs
+{
+    public class EducationDTO
+    {
+        public int ResumeId { get; set; }
+        public string InstituteName { get; set; }
+        public string Degree { get; set; }
+        public string FieldOfStudy { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
