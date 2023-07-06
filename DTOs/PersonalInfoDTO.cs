137bd367a7e85abb1738e580100a351f2e58e80a
georgikk314 <gogomen2005@gmail.com>
2023-07-07 01:38:32 +0300

    add PersonalInfo DTO

---
+++ DTOs/PersonalInfoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Online_CV_Builder.DTOs
+{
+    public class PersonalInfoDTO
+    {
+        public int ResumeId { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
