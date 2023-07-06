802527fe616613c0c9fa6f81ca6ca8cc6de11f26
georgikk314 <gogomen2005@gmail.com>
2023-07-07 02:48:05 +0300

    make the validation for location

--- DTOs/ResumeRelatedDTOs/LocationDTO.cs
+++ DTOs/ResumeRelatedDTOs/LocationDTO.cs
@@ -1,10 +1,19 @@
-﻿namespace Online_CV_Builder.DTOs.ResumeRelatedDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_CV_Builder.DTOs.ResumeRelatedDTOs
 {
     public class LocationDTO
     {
         public int ResumeId { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        [StringLength(100, ErrorMessage = "City cannot exceed 100 characters.")]
         public string City { get; set; }
+
+        [StringLength(100, ErrorMessage = "State cannot exceed 100 characters.")]
         public string? State { get; set; }
+
+        [StringLength(100, ErrorMessage = "Country cannot exceed 100 characters.")]
         public string Country { get; set; }
     }
 }
