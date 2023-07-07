68e9ee31121f24e976919241640da39b7dc27f9a
Ivan Manolov <i.manolov05@gmail.com>
2023-07-07 18:39:21 +0300

    register and login methods

---
+++ Controllers/LogoutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Online_CV_Builder.Controllers
+{
+    [Route("api/logout")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
