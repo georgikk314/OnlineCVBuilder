5b7f863c6ab6fb7cfde43194ee02bc4300e047c5
Ivan Manolov <i.manolov05@gmail.com>
2023-07-11 10:33:04 +0300

    authentication DTOs

---
+++ DTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_CV_Builder.Models
+{
+    public class LoginDTO
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MinLength(10), MaxLength(50)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ DTOs/RegisterDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace Online_CV_Builder.Models
+{
+    public class RegisterDTO
+    {
+        public string Endpoint = "/register";
+        public int Id { get; set; }
+        [Required, NotNull]
+        [StringLength(100)]
+        public string Username { get; set; } = string.Empty;
+        [Required, EmailAddress, NotNull]
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+        [Required, NotNull, MinLength(10), MaxLength(50)]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password"), NotNull, MinLength(10), MaxLength(50)]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
