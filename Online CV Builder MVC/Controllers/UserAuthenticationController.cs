e2cb15f772a53bd90938577a7f83d4e63bb4987a
Georgi Kostadinov <gogomen2005@gmail.com>
2023-07-18 16:00:05 +0300

    add loginviewmodel

---
+++ Online CV Builder MVC/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_CV_Builder_MVC.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:5096/api");
+        private readonly HttpClient _client;
+
+        public UserAuthenticationController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}
