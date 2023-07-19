0d314784ecee5bdf4ba36c3ad749d08af02621d1
Ivan Manolov <i.manolov05@gmail.com>
2023-07-20 01:02:01 +0300

    fixed shared page view design

--- Online CV Builder MVC/Controllers/HomeController.cs
+++ Online CV Builder MVC/Controllers/HomeController.cs
@@ -12,23 +12,18 @@ namespace Online_CV_Builder_MVC.Controllers
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult ToResume()
         {
             return View();
         }
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        //fffffff
     }
 }
